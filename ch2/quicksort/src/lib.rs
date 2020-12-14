c7516ec74bf9f4d0831b8f8e22f2e8162ef0e9af
ebwb <ebwbernard@gmail.com>
2020-12-14 01:01:46 -0600

    Add dependency on rand. Flesh out algorithm.

--- ch2/quicksort/src/lib.rs
+++ ch2/quicksort/src/lib.rs
@@ -1,7 +1,47 @@
-pub fn sort(mut list: &Vec<i32>) -> &Vec<i32> {
-    list
+use rand::prelude::*;
+
+pub fn sort(list: &mut [i32], end: usize) {
+
+    println!("list: {:?}", list);
+    println!("end: {:?}", end);
+    
+    if end <= 1 {
+	println!("found end, returning");
+        return;
+    }
+    
+    let mut rng = thread_rng();
+    let random_index : usize = rng.gen_range(0, end);
+    let _random_index = end;
+    println!("pivot: {}", random_index);
+    
+    swap(list, 0, random_index);
+    println!("first swap: {:?}", list);
+    let mut last = 0;
+
+    for i in 1..end {
+	if list[i] < list[0] {
+	    last += 1;
+	    swap(list, last, i);
+	}
+    }
+
+    
+    println!("last: {:?}", last);
+    swap(list, 0, last);
+    println!("now list: {:?}", list);
+    println!("sorting lower half");
+    sort(&mut list[0..last], last);
+    println!("sorting upper half");
+    sort(&mut list[last..end], end-last);
 }
 
+// assumes both i and j are valid elements in list. panics if not.
+fn swap(list: &mut [i32], i: usize, j: usize) {
+    let temp = list[i];
+    list[i] = list[j];
+    list[j] = temp;
+}
 
 #[allow(non_snake_case)]
 #[cfg(test)]
@@ -10,39 +50,70 @@ mod tests {
 
     #[test]
     fn emptyList_isSorted() {
-        let empty_list: Vec<i32> = vec![];
+        let mut empty_list: [i32; 0] = [];
+	let length = empty_list.len();
 
-        assert_eq!(Vec::<i32>::new(), *sort(&empty_list));
+	sort(&mut empty_list, length);
+
+        assert_eq!(Vec::<i32>::new(), empty_list);
     }
 
     #[test]
     fn listOfOne_isSorted() {
-        let list_of_one: Vec<i32> = vec![5];
+        let mut list_of_one: [i32; 1] = [5];
+	let length = list_of_one.len();
 
-        assert_eq!(list_of_one, *sort(&list_of_one));
+	sort(&mut list_of_one, length);
+        assert_eq!([5], list_of_one);
     }
 
     #[test]
     fn smallList_sortsAscending() {
-	let small_list : Vec<i32> = vec![6, 0, 1];
-	let expected_list : Vec<i32> = vec![0, 1, 6];
-	
-	assert_eq!(expected_list, *sort(&small_list));
+        let mut small_list: [i32; 3] = [6, 0, 1];
+        let expected_list: [i32; 3] = [0, 1, 6];
+	let length = small_list.len();
+
+	sort(&mut small_list, length);
+        assert_eq!(expected_list, small_list);
     }
 
     #[test]
     fn sortedList_remainsSorted() {
-	let sorted_list : Vec<i32> = vec![1, 13, 193, 943, 93139];
-	let expected_list : Vec<i32> = vec![1, 13, 193, 943, 93139];
+        let mut sorted_list: [i32; 5] = [1, 13, 193, 943, 93139];
+        let expected_list: [i32; 5] = [1, 13, 193, 943, 93139];
+	let length = sorted_list.len();
 
-	assert_eq!(expected_list, *sort(&sorted_list));
+	sort(&mut sorted_list, length);
+        assert_eq!(expected_list, sorted_list);
     }
 
     #[test]
     fn reverseList_sortsAscending() {
-	let reverse_list : Vec<i32> = vec![999, 943, 931, 64, 32, 1];
-	let expected_list : Vec<i32> = vec![1, 32, 64, 931, 943, 999];
+        let mut reverse_list: [i32; 6] = [999, 943, 931, 64, 32, 1];
+        let expected_list: [i32; 6] = [1, 32, 64, 931, 943, 999];
+	let length = reverse_list.len();
+
+	sort(&mut reverse_list, length);
 	
-	assert_eq!(expected_list, *sort(&reverse_list));
+        assert_eq!(expected_list, reverse_list);
+    }
+
+    #[test]
+    fn smallList_swap() {
+	let mut small_list : [i32; 2] = [0, 1];
+	let expected_list : [i32; 2] = [1, 0];
+	swap(&mut small_list, 0, 1);
+
+	assert_eq!(expected_list, small_list);
+    }
+
+    #[test]
+    fn largerList_swap() {
+	let mut larger_list : [i32; 8] = [1, 2, 3, 4, 5, 6, 7, 8];
+	let expected_list : [i32; 8] = [1, 2, 7, 4, 5, 6, 3, 8];
+
+	swap(&mut larger_list, 2, 6);
+
+	assert_eq!(expected_list, larger_list);
     }
 }
